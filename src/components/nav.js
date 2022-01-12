4a0f3ea029f52c50966e11b7613b9f6af58d4fc0
caovandanh <caovandanh2002@gmail.com>
2022-01-12 13:19:39 +0700

    feat: Update lab2

---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+const Nav = {
+    print() {
+        return /* html */`
+        <nav>
+            <ul class="flex">
+                <li><a href="/" class="block py-3 px-4 text-white hover:bg-blue-500">Trang chủ</a></li>
+                <li><a href="/about" class="block py-3 px-4 text-white hover:bg-blue-500">Chương trình đào tạo</a></li>
+                <li><a href="/product" class="block py-3 px-4 text-white hover:bg-blue-500">Góc sinh viên</a></li>
+                <li><a href="/product1" class="block py-3 px-4 text-white hover:bg-blue-500">Tuyển sinh</a></li>
+                <li><a href="/product2" class="block py-3 px-4 text-white hover:bg-blue-500">Tuyển dụng</a></li>
+            </ul>
+        </nav>`;
+    },
+};
+export default Nav;
