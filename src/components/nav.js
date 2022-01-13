497410fb2aa094c1159e939e3a747948294993f2
caovandanh <caovandanh2002@gmail.com>
2022-01-13 16:56:26 +0700

    feat: update seach

--- src/components/nav.js
+++ src/components/nav.js
@@ -9,6 +9,10 @@ const Nav = {
                 <li><a href="/product1" class="block py-3 px-4 text-white hover:bg-blue-500">Tuyển sinh</a></li>
                 <li><a href="/product2" class="block py-3 px-4 text-white hover:bg-blue-500">Tuyển dụng</a></li>
             </ul>
+            <div class="seach">
+                <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-gray-300 placeholder-gray-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 inline-block w-70 rounded-md sm:text-sm focus:ring-1"/>
+                <button class="rounded-full mt-1 px-3 py-2 bg-blue-700 text-white">Save Changes</button>
+            </div>
         </nav>`;
     },
 };
